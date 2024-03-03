d2f949873e8100bf547c15b399448a1b8d7538bb
Nuhash Jobayed <work.nuhash@gmail.com>
2024-03-04 03:41:58 +0600

    somechanges

--- src/App.js
+++ src/App.js
@@ -19,21 +19,22 @@ function App() {
 
   
      
-    <ScrollControls pages={4} damping={0.2}>
+    <ScrollControls pages={3} damping={0.2}>
         
     <Model/>
-               <Scroll html>
+               <Scroll html >
+                <div className='div'>
               <section className='sec'>
-              <h1>NUHASH</h1>
+              <h1>宋朝</h1>
               </section>
               <section className='sec'>
-              <p>Chineese King OURHAZ</p>
+              <p>宋朝（960年-1279年）是中国历史上的一个重要时期。它分为北宋和南宋两个时期。北宋时期，政治稳定，经济繁荣，文化发展达到了巅峰。北宋最为著名的是开创性的科技和文化成就，如活字印刷术和宋词等。南宋时期，虽然面临来自金朝的威胁和南方的地方势力，但南宋在文化和艺术领域仍然取得了很多成就。然而，宋朝最终于1279年灭亡于蒙古的入侵，结束了长达三百多年的统治。尽管如此，宋朝留下了深远的影响，其文化、科技和政治制度对中国历史产生了重要影响</p>
               </section>
               <section className='sec'>
-              <h1>WILL IT WORK???</h1>
+              <h1>伟大的中国君王</h1>
               </section>
             
-              
+              </div>
     
             </Scroll>
     
